04d2a2df45dcc32e9acfb9d09588650d2e223b46
Sheyar Ali <ali.sheyar@gmail.com>
2017-10-07 22:25:06 +0200

    Add connect and disconnect to the app

--- server/server.js
+++ server/server.js
@@ -1,14 +1,25 @@
-const path = require('path');
+const path = require('path'); // to customize our paths
+const http = require('http'); // express uses https, but to add socket.io to the app we have configure it with http by ourselves
 const express = require('express');
+const socketIO = require('socket.io');//this library has frontend and backend
 
 const publicPath = path.join(__dirname, '../public');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000; //for heroku to set the port or default one 3000
 var app = express();
+var server = http.createServer(app);// create http server
+var io = socketIO(server); //here socket can access to the server and using io we can emit and listen to events
+
 
 app.use(express.static(publicPath));
 
+io.on('connection', (socket) => { // to rigester connection listener
+    console.log('New user is connected');
 
+    socket.on('disconnect', () => {
+        console.log('User is disconnected');
+    });
+});
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
